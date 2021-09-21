543ac90c0e336ae6be98653cb2bec027c0bb94cf
Mauricio Poblete <mpoblete.s3@gmail.com>
2021-09-20 23:51:54 -0300

    refactorize code and added tests

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ### Ecommerce system
 
 This project is part of my studies. Only made for study purposes.
+
+### Changelog
+
+(20-09-2021)
+- Refactorize code to reduce controller and add functions on models.
+- Added test for models and new functions.
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,15 @@
 class Payment < ApplicationRecord
   belongs_to :order
   belongs_to :payment_method
+
+  def close!
+    ActiveRecord::Base.transaction do
+      order.complete
+      complete
+    end
+  end
+
+  def complete
+    update_attribute(:state, "completed")
+  end
 end
